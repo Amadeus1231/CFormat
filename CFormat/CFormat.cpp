da1c2d5ffb56aa9c40c48f7568a466809e5046c6
Shiraz <shiraz.2002@mail.ru>
2022-06-17 16:35:34 +0300

    first

---
+++ CFormat/CFormat.cpp
@@ -0,0 +1,27 @@
+﻿
+#include <cstdlib>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+               // Первая строка -- особый случай.
+               string last;
+               if (!getline(cin, last))
+                              return EXIT_FAILURE;          // Не смогли прочитать и одной строки.
+
+               cout << last << endl;          // Вывести первую строку.
+
+               // Продолжить работу с остатком файла.
+               for (string next; getline(cin, next);)
+               {
+                              if (last != next)
+                              {
+                                             cout << next << endl;
+                                             last = next;
+                              }
+               }
+
+               return EXIT_SUCCESS;
+}
