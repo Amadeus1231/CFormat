a2bced717cc318b02f32dc215ddb0912adec6143
Shiraz <shiraz.2002@mail.ru>
2022-06-23 19:25:31 +0300

    Добавлены файлы

---
+++ check_format.py
@@ -0,0 +1,12 @@
+import subprocess
+import os
+from sys import argv
+with open("result.txt", "w") as file:
+    subprocess.run(f'clang-format {argv[1]} -n', stdout=None, stderr=file)
+with open("result.txt", "r") as file:
+    text = file.read()
+    if text:
+        print("файл не соотв. критериям форматирования")
+    else:
+        print("файл отформатирован")
+os.remove("result.txt")
