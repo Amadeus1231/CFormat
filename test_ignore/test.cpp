3ed8cfc1fc7aeabde8802112719a2f62e975f433
Shiraz <shiraz.2002@mail.ru>
2022-07-10 13:13:50 +0300

    Изменение

---
+++ test_ignore/test.cpp
@@ -0,0 +1,23 @@
+int on_msg_begin(http_parser* apParser)
+{
+  TApcHttpParser* pObj = (TApcHttpParser*)(apParser->data);
+  if (!pObj) {
+    // некуда логировать :(
+    return -33;
+  }
+
+  return pObj->onMsgBegin();
+}
+//---------------------------------------------------------------------------
+
+int on_msg_complete(http_parser* apParser)
+{
+  TApcHttpParser* pObj = (TApcHttpParser*)(apParser->data);
+  if (!pObj) {
+    // некуда логировать :(
+          return -33;
+  }
+
+  return pObj->onMsgComplete();
+}
+//---------------------------------------------------------------------------
